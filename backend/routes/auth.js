f9c7eef0c04de7bf8b183dc9dbe4470520c74c90
Inalik <hernandezmaicol106@gmail.com>
2026-09-11 07:28:35 -0500

    googleauthlisto

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { registrar, iniciarSesion } from '../controllers/auth.js';
+import { autenticarConGoogle } from "../controllers/googleauth.js";
 
 const router = Router();
 
 router.post('/registro', registrar);
 router.post('/login', iniciarSesion);
+// Endpoint: POST /api/auth/google
+router.post("/google", autenticarConGoogle);
 
 export default router;
